9827de126d05133ecb270ecaa51e7bc11561238b
Sorraimi <sorraimi.rivas@hotmail.com>
2023-05-18 16:07:18 -0400

    Added tweet component with scrollable view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,23 @@
-import { StyleSheet } from 'react-native';
+import { Alert, FlatList, Image, StyleSheet } from "react-native";
+import { Text, View } from "../../components/Themed";
+import tweets from "../../assets/data/tweets";
+import Tweet from "../../components/Tweet";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+const tweet = tweets[0];
 
 export default function TabOneScreen() {
+  const handleOnPress = () => {
+    Alert.alert("Pressed");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <>
+      <FlatList
+        data={tweets}
+        showsVerticalScrollIndicator={false}
+        bounces={false}
+        renderItem={({ item }) => <Tweet tweet={item} />}
+      />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
